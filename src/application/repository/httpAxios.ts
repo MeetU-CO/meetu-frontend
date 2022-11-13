c88f92fa4c5547ee68cb23ada4733a1338d81654
Jonathan Buitrago <jonathanbuitragoroncancio50@gmail.com>
2022-11-13 18:49:25 -0500

    Rename repository folder

--- src/application/repository/httpAxios.ts
+++ src/application/repository/httpAxios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { http } from "../../domain/repositories/http";
+import { http } from "../../domain/repository/http";
 
 const headers = {
   "Content-type": "application/json",
