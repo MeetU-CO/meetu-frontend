33875ba5ac50f42eb88008a6512c3817955fc7e2
Jonathan Buitrago <jonathanbuitragoroncancio50@gmail.com>
2023-01-08 18:04:00 -0500

    Add auth-callback page

--- src/container/App.tsx
+++ src/container/App.tsx
@@ -1,11 +1,15 @@
+import "bootstrap-icons/font/bootstrap-icons.css";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "swiper/css/bundle";
 
 import Layout from "../modules/presentation/templates/Layout/Layout";
 
 import AboutUs from "../modules/presentation/pages/AboutUs/AboutUs";
+import AuthCallback from "../modules/presentation/pages/AuthCallback/AuthCallback";
 import Home from "../modules/presentation/pages/Home/Home";
 import HowItWorks from "../modules/presentation/pages/HowItWorks/HowItWorks";
 import Login from "../modules/presentation/pages/Login/Login";
@@ -19,9 +23,6 @@ import { login } from "../modules/infraestructure/slices/AuthSlice";
 import "./App.scss";
 import "./colors.variables.css";
 import "./normalize.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
-import "react-toastify/dist/ReactToastify.css";
-import "swiper/css/bundle";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -54,6 +55,7 @@ const App = () => {
           <Route path="sobre-nosotros" element={<AboutUs />} />
           <Route path="login" element={<Login />} />
           <Route path="Signup" element={<Signup />} />
+          <Route path="auth-callback" element={<AuthCallback />} />
         </Routes>
       </Layout>
     </div>
---
+++ src/modules/presentation/templates/AuthCallback/AuthCallback.tsx
@@ -0,0 +1,9 @@
+import "./AuthCallback.scss";
+
+interface IAuthCallback {}
+
+const AuthCallback = ({}: IAuthCallback) => {
+  return <div className="authCallback">Auth Callback</div>;
+};
+
+export default AuthCallback;
